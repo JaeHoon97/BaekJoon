95c8dc4ae2c2639e3cb2b68c32a4b8b5556b8f87
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2023-12-24 00:33:39 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int num;
+
+    cin >> num;
+
+    for (int i = 1; i <= num; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
