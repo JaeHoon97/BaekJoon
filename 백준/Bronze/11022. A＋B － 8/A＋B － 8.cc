2d1aace4efabea7467c72b8f5011e70238f26a26
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2023-12-24 00:27:31 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int count, a, b;
+
+    cin >> count;
+
+    for (int i = 1; i <= count; i++) {
+        cin >> a >> b;
+        cout << "Case #" << i << ": " << a<< " + " << b << " = " << a + b << "\n";
+    }
+
+    return 0;
+}
