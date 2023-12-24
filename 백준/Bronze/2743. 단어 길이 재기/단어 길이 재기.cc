8700359bd7725d8891d15da3f53f29aeedd85d1a
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2023-12-24 23:10:42 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    // cin.tie(NULL);
+    // ios_base::sync_with_stdio(false);
+
+    string s;
+
+    getline(cin, s);
+
+    cout << s.length() << endl;
+
+    return 0;
+}
