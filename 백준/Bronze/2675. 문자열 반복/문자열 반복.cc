2723622dfec522420966ce0aaa417990036873fa
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2023-12-26 00:50:41 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -8,23 +8,17 @@ int main(void) {
     int t, r;          // 테스트 케이스 t, 반복 횟수 r
     string str;        // 반복할 문자열 str
     vector<string> v;  // 완성된 문자열 v
-
     cin >> t;
-
     for (int i = 0; i < t; i++) {
         cin >> r >> str;
-
         string str2;
-
         for (int j = 0; j < (str.length() * r); j++) {
             str2.push_back(str[j / r]);
         }
         v.push_back(str2);
     }
-
     for (int i = 0; i < v.size(); i++) {
         cout << v[i] << "\n";
     }
-
     return 0;
 }
