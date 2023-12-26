1971bf67c9b2119b2acee8b693fd31fe593f0d3c
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2023-12-26 17:13:45 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    string data;
+    vector<string> str;
+
+    while (true) {
+        getline(cin, data);
+        if (cin.eof()) {
+            break;
+        }
+        str.push_back(data);
+    }
+
+    for (int i = 0; i < str.size(); i++) {
+        cout << str[i] << endl;
+    }
+
+    return 0;
+}
