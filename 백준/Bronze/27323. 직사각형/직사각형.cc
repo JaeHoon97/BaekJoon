1d1e66fef872aced90105774b2f1489998b33687
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2023-12-30 21:10:14 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int x, y;
+
+    cin >> x >> y;
+
+    cout << x * y << endl; 
+
+    return 0;
+}
