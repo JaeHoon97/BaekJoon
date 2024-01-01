2d50a2e466922a66acc75e9f2992dd9630013a82
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-01-02 02:43:58 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    int n, k;
+
+    cin >> n >> k;
+
+    vector<int> v(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    cout << v[n - k] << '\n';
+
+    return 0;
+}
