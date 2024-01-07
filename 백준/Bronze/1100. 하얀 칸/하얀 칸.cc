751e4e400b2fd2da2b3a98a750983f48b3a9b72b
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-01-08 00:09:35 +0900

    [Bronze II] Title: 하얀 칸, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.cc
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    char arr[8][8] = {' '};
+    int white = 0;
+
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (i % 2 == 0 && j % 2 == 0 && arr[i][j] == 'F') {
+                white++;
+            }
+            if (i % 2 != 0 && j % 2 != 0 && arr[i][j] == 'F') {
+                white++;
+            }
+        }
+    }
+
+    cout << white << '\n';
+
+    return 0;
+}
