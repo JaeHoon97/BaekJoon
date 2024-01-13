7bb6c544d1f8de82facb79d0c4c660976bacf348
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-01-13 22:49:34 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    cout  << "강한친구 대한육군" <<endl;
+    cout  << "강한친구 대한육군" <<endl;
+    
+    return 0;
+}
