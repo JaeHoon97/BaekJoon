a3ac6a55917e8e3eca66522cb80851cc7c9a7383
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-01-13 23:29:46 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int fibonacci(int n) {
+    if (n >= 2) {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    } else {
+        return n;
+    }
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    int n, cnt = 0;
+    long long f[46] = {0};
+    f[0] = 0, f[1] = 1;
+
+    cin >> n;
+
+    for (int i = 2; i < 46; i++) {
+        f[i] = f[i - 1] + f[i - 2];
+    }
+
+    cout << f[n] << '\n';
+
+    return 0;
+}
