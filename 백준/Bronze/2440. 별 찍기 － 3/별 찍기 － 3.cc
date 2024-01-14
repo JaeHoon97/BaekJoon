f37e5fea7e497375e78d2e43111d2b764362c8f0
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-01-14 23:24:34 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    int n;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-i; j++) {
+            cout << '*';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
