f465dccd31f8388761d7cae6afc2fd7ee152f122
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-01-14 23:21:12 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    cout << "117" << endl;
+
+    cout << "stitch8971" << endl;
+
+
+    return 0;
+}
