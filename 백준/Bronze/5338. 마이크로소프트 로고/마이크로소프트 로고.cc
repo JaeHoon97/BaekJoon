ac6207d1c3e5f8d2d914bcddb9df61f808b6f69a
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-01-24 14:26:54 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    cout << "       _.-;;-._" << endl;
+    cout << "'-..-'|   ||   |" << endl;
+    cout << "'-..-'|_.-;;-._|" << endl;
+    cout << "'-..-'|   ||   |" << endl;
+    cout << "'-..-'|_.-''-._|" << endl;
+
+    return 0;
+}
