15732793ad0f2eefb0376ca6b6e94bea8493d528
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-01-30 23:57:37 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+
+
+	cout << "     /~\\" <<'\n';
+	cout << "    ( oo|"<<'\n';
+	cout << "    _\\=/_"<<'\n';
+	cout << "   /  _  \\" <<'\n';	
+	cout << "  //|/.\\|\\\\ " << '\n';
+	cout << " ||  \\ /  ||" << '\n';
+	cout << "============" << '\n';
+	cout << "|          |" << '\n';
+	cout << "|          |" << '\n';
+	cout << "|          |" << '\n';
+
+
+    return 0;
+}
