85c2b75467a7bad4eeb58b0e1e797c0cc2ad6139
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-02-04 23:22:54 +0900

    [Bronze V] Title: NFC West vs North, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.cc
@@ -0,0 +1,26 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    cout << "NFC West       W   L  T" << endl;
+    cout << "-----------------------" << endl;
+    cout << "Seattle        13  3  0" << endl;
+    cout << "San Francisco  12  4  0" << endl;
+    cout << "Arizona        10  6  0" << endl;
+    cout << "St. Louis      7   9  0" << endl;
+    cout << endl;
+    cout << "NFC North      W   L  T" << endl;
+    cout << "-----------------------" << endl;
+    cout << "Green Bay      8   7  1" << endl;
+    cout << "Chicago        8   8  0" << endl;
+    cout << "Detroit        7   9  0" << endl;
+    cout << "Minnesota      5  10  1" << endl;
+
+    return 0;
+}
