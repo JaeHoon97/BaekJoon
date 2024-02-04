648b5be16394882a0cc5285b24627e32bb3b9299
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-02-04 23:23:37 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    cout << "I'm Sexy" << endl;
+
+
+    return 0;
+}
