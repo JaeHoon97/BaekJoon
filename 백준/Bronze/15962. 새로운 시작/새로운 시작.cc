a8facfc0c7dc422b6b4c2bd8ea3a33df4fe13558
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-02-05 22:54:30 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    cout << "파이팅!!" << endl;
+
+    return 0;
+}
