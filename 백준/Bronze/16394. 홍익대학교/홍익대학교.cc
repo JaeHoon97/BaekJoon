75bbd97cde4133c05c94b8169bd404996166b611
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-02-09 23:05:37 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cout << n - 1946 << endl;
+
+
+    return 0;
+}
