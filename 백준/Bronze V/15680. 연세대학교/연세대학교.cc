be02e801a326e1b22a159e511f0e43f6059e7681
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-02-14 23:39:57 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze V/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    
+    int n;
+    
+    cin >> n;
+    
+    n == 0 ? cout << "YONSEI" : cout << "Leading the Way to the Future";
+
+    return 0;
+}
