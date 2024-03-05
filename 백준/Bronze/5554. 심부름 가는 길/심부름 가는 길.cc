cd0cc35f5ff2219c44541d021bbeff92501cbafa
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-03-05 23:36:45 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int a, b, c, d;
+
+	cin >> a >> b >> c >> d;
+
+	cout << (a + b + c + d) / 60 << endl;
+	cout << (a + b + c + d) % 60 << endl;
+
+	return 0;
+}
