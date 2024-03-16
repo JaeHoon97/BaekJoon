3c1ba784b6610321c4531bc9c5f4534465abe7dc
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-03-16 22:53:36 +0900

    [level 0] Title: 약수 구하기, Time: 0.01 ms, Memory: 3.61 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    
+    for(int i=1; i<=n; i++) {
+        if(n % i == 0){
+            answer.push_back(i);
+        }
+    }
+    
+    sort(answer.begin(), answer.end());
+    
+    return answer;
+}
