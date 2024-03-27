fb0044e2c0a4774fffc1d15f12a2c16ff5e30e18
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-03-28 01:32:45 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 3684 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    priority_queue<long long> pq;
+    long long N, num;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> num;
+
+        if (num == 0) { // x가 자연수가 아닐 경우
+            if (!pq.empty()) { // 최대힙이 비어있지 않을 경우
+                cout << pq.top() << '\n'; // 가장 큰 값 출력
+                pq.pop(); // 가장 큰 값 제거
+            } else { // 최대힙이 비어있을 경우
+                cout << 0 << '\n';
+            }
+        } else {  // x가 자연수일 경우
+            pq.push(num); // 자연수 추가
+        }
+    }
+
+    return 0;
+}
