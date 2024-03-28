93bc19a52083c5e02514908a40716ef2a1977011
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-03-28 23:50:05 +0900

    [Unrated] Title: 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/14918. 더하기/더하기.cc
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a + b << endl;
+
+    return 0;
+}
