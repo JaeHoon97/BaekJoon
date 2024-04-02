a5a71275126d5f70fee7f136b7a1c8fd9a3e8339
JaeHoon97 <108669890+JaeHoon97@users.noreply.github.com>
2024-04-02 20:27:51 +0900

    [Silver III] Title: 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.cc
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    long long N;
+
+    while (cin >> N) {
+        long long i = 1, count = 1;
+
+        while (true) {
+            if (i % N == 0) {
+                cout << count << '\n';
+                break;
+            } else {
+                i = (i * 10 + 1);
+                i %= N;
+                count++;
+            }
+        }
+
+    }
+
+    return 0;
+}
