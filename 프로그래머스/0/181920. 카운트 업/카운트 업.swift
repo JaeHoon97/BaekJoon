e52bc598bf5dbf786045db6ce900d8d84a3e1a92
JaeHoon <108669890+JaeHoon97@users.noreply.github.com>
2024-06-23 02:49:53 +0900

    [level 0] Title: 카운트 업, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ start_num:Int, _ end_num:Int) -> [Int] {
+    
+    var array = [Int]()
+    
+    for i in start_num...end_num{
+        array.append(i)
+    }
+    
+    
+    
+    
+    return array
+}
