06283a1cff40dc4d90c64cca3fa65a9e3fdd54e7
JaeHoon <108669890+JaeHoon97@users.noreply.github.com>
2024-07-20 00:13:46 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.07 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.swift
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.swift
@@ -1,8 +1,4 @@
-import Foundation
-
 func solution(_ num_list:[Int]) -> Int {
-    
-    if num_list.isEmpty { return -1}
     
     for (index, value) in num_list.enumerated() {
         if value < 0 { return index }
