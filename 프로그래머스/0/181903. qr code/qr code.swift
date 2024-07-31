795da8babb95bd8f318840b5429960b0092b65da
JaeHoon <108669890+JaeHoon97@users.noreply.github.com>
2024-07-31 21:39:59 +0900

    [level 0] Title: qr code, Time: 0.45 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+func solution(_ q:Int, _ r:Int, _ code:String) -> String {
+    
+    return code.enumerated().filter { $0.offset % q == r }.map { String($0.element) }.joined()
+}
