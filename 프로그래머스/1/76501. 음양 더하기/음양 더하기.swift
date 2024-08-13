c56d6a29ae5e2e3e5c486de48fd9fc4ecdda8c5e
JaeHoon <108669890+JaeHoon97@users.noreply.github.com>
2024-08-13 16:41:09 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+    var result = 0
+    for i in 0..<absolutes.count {
+        if signs[i] { 
+            result += absolutes[i]
+        } else {
+            result -= absolutes[i]
+        }
+    }
+    
+    return result
+}
