f949fdaf53265eaba37d33a1463d40840880cc88
JaeHoon <108669890+JaeHoon97@users.noreply.github.com>
2024-08-15 13:41:11 +0900

    [level 1] Title: 직사각형 별찍기, Time: 91.71 ms, Memory: 14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+
+for i in 0..<b {
+    print(String(Array(repeating: "*", count: a)))
+}
+
