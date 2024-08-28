0d558102f2c1f10ec9d7db211b8bef41c8df7f6c
JaeHoon <108669890+JaeHoon97@users.noreply.github.com>
2024-08-28 13:40:40 +0900

    [level 2] Title: 올바른 괄호, Time: 22.17 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
@@ -4,21 +4,15 @@ func solution(_ s:String) -> Bool {
     var stack: [Character] = []
     
     for i in s.indices {
-        
         if s[i] == ")" {
-            if stack.isEmpty {
-                return false
-            } else {
-                stack.removeLast()
-            }
+            guard !stack.isEmpty else { return false }
+            stack.removeLast()
         } else {
             stack.append(s[i])
         }
     }
     
-    if !stack.isEmpty {
-        return false
-    }
+    if !stack.isEmpty { return false }
     
     return true
 }
