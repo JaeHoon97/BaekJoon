5cbf3be1b33e25ea05992a210d66adca47373a46
JaeHoon <108669890+JaeHoon97@users.noreply.github.com>
2024-09-10 21:09:32 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
@@ -1,20 +1,8 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var str: String = ""
-    var num: Int = n
-    var val = 1
     
-    while num >= 3 {
-        str += String(num % 3)
-        num = num / 3
-    }
+    var tenToThree = String(n, radix: 3)
     
-    str += String(num)
-    
-    return str.reversed().map { ch in
-        var result =  Int(String(ch))! * val
-        val *= 3
-        return result
-    }.reduce(0, +)
+    return Int(String(tenToThree.reversed()), radix: 3)!
 }
