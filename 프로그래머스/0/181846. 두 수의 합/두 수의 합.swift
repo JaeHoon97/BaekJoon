2029e48ae1c73c7f6d1a3294a5bf8ef89fc76b2c
JaeHoon <108669890+JaeHoon97@users.noreply.github.com>
2024-09-23 21:52:38 +0900

    [level 0] Title: 두 수의 합, Time: 116.33 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.swift
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.swift
@@ -1,18 +1,20 @@
 import Foundation
 
 func solution(_ a:String, _ b:String) -> String {
-    var num1 = a
-    var num2 = b
-    var remain = 0
+    let num1: [Character] = Array(a).reversed()
+    let num2: [Character] = Array(b).reversed()
     var result = ""
+    var share = 0
+    let size = max(num1.count, num2.count)
     
-    
-    while true {
-        let num = Int(String((num1.popLast() ?? "0")))! + Int(String((num2.popLast() ?? "0")))! + remain
-        remain = num / 10
-        result.append(String(num % 10))
-        if num1.isEmpty && num2.isEmpty && remain == 0 { break }
+    for i in 0..<size {
+        let n1 = (i < num1.count) ? Int(String(num1[i]))! : 0
+        let n2 = (i < num2.count) ? Int(String(num2[i]))! : 0
+        result.append(String((n1 + n2 + share) % 10))
+        share = (n1 + n2 + share) / 10
     }
+    
+    if share != 0 { result.append(String(share))}
     
     return String(result.reversed())
 }
