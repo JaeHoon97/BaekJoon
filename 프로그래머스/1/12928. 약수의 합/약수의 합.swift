51416e6e65228d3691e09d60e0f3e97a3c5f2e97
JaeHoon <108669890+JaeHoon97@users.noreply.github.com>
2024-09-23 22:16:37 +0900

    [level 1] Title: 약수의 합, Time: 0.08 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.swift
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.swift
@@ -1,6 +1,18 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    if n == 0 { return 0 }
-    return (1...n).filter { n.isMultiple(of: $0) }.reduce(0, +)
+    
+    if n == 0 || n == 1 { return n }
+    
+    let sqaureRoot = sqrt(Double(n))
+    var result: Set<Int> = []
+    
+    for i in 1...Int(sqaureRoot) {
+        if n.isMultiple(of: i) {
+            result.insert(i)
+            result.insert(n / i)
+        }
+    }
+    
+    return result.reduce(0, +)
 }
