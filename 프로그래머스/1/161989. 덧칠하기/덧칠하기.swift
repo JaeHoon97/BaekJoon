b94fe0b2479875f9d1ec68b49335cd3e9b93b64e
JaeHoon <108669890+JaeHoon97@users.noreply.github.com>
2024-09-25 17:46:45 +0900

    [level 1] Title: 덧칠하기, Time: 11.77 ms, Memory: 19.8 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.swift
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.swift
@@ -1,23 +1,15 @@
 import Foundation
 
 func solution(_ n:Int, _ m:Int, _ section:[Int]) -> Int {
-    var remain: [Bool] = Array(repeating: true, count: n+1)
-    var result = 0
-    
-    section.forEach {
-        remain[$0] = false
-    }
-    
-    for i in section {
-        if !remain[i] {
-            for j in i..<i+m {
-                if (j <= n) && !remain[j] {
-                    remain[j] = true
-                }
-            }
-            result += 1
+    var now = section[0] // 페인트를 칠해야되는 첫번째 섹션
+    var counter = 0 // 페인트 칠한 수
+
+    for s in section {
+        if s >= now { // 현재 위치보다 페인트 칠해야하는 섹션이 뒤에 있을 경우
+            counter += 1 // 페인트 칠한 수 +1 증가
+            now = s + m // 다음 위치 갱신
         }
     }
-    
-    return result
+
+    return counter
 }
