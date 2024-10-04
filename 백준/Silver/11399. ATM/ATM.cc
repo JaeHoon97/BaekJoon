c6dc944408bd393b03c5147c9f51098096f4a3e4
JaeHoon <108669890+JaeHoon97@users.noreply.github.com>
2024-10-04 22:47:31 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cc
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -7,11 +7,11 @@ using namespace std;
 int main(void) {
     ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
 
-    int person, min, result = 0; // 인원, 분, 최소시간
+    int person, min, result = 0;  // 인원, 분, 최소시간
 
     cin >> person;
 
-    int wait[person]; // 각 순서의 사람이 기다려야되는 시간
+    int wait[person];  // 각 순서의 사람이 기다려야되는 시간
 
     for (int i = 0; i < person; i++) {
         cin >> min;
@@ -21,9 +21,7 @@ int main(void) {
     sort(wait, wait + person);
 
     for (int i = 0; i < person; i++) {
-        for (int j = 0; j <= i; j++) {
-            result += wait[j];
-        }
+        result += wait[i] * (person - i);
     }
 
     cout << result << endl;
