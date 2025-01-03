29bf18ef52fcf886a632f0ffda03c255f710633a
JaeHoon <108669890+JaeHoon97@users.noreply.github.com>
2025-01-04 03:37:56 +0900

    [level 1] Title: 내적, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    int answer = 0;
+
+    for (int i = 0; i < a.size(); i++) {
+        answer += a[i] * b[i];
+    }
+
+    return answer;
+}
