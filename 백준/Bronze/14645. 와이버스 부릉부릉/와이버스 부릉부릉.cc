02fc740047696c596f9cd079e7fd8493970705a2
7aeHoon <108669890+JaeHoon97@users.noreply.github.com>
2025-01-17 02:14:00 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,22 @@
+// // cd "/Users/jaehoon/Desktop/c++/" && g++ -std=c++17 *.cpp -o main && "/Users/jaehoon/Desktop/c++/"main
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    int N, K;
+
+    cin >> N >> K;
+
+    for (int i = 0; i < N; i++) {
+        int a, b;
+        cin >> a >> b;
+    }
+
+    cout << "비와이" << '\n';
+
+    return 0;
+}
