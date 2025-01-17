2d837cdb05bc58220c9c6a7e106bccef3a109767
7aeHoon <108669890+JaeHoon97@users.noreply.github.com>
2025-01-18 00:29:09 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int chicken) {
+    int answer = 0;
+
+    while(chicken >= 10) {
+        answer += chicken / 10;
+        chicken = chicken / 10 + chicken % 10;
+    }
+
+    return answer;
+}
