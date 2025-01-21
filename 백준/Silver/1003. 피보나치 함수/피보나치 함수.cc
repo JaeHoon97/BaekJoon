2d334dfbca93935ff326d0e7bee17c45b476f0b2
7aeHoon <108669890+JaeHoon97@users.noreply.github.com>
2025-01-22 00:41:53 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int T, N;
+vector<pair<long long, long long>> fibo(45, {0, 0});
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+
+    fibo[0] = {1, 0};
+    fibo[1] = {0, 1};
+
+    for (int i = 2; i <= 40; i++) {
+        long long first = fibo[i - 1].first + fibo[i - 2].first;
+        long long second = fibo[i - 1].second + fibo[i - 2].second;
+        fibo[i] = {first, second};
+    }
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        cin >> N;
+        cout << fibo[N].first << " " << fibo[N].second << '\n';
+    }
+
+    return 0;
+}
