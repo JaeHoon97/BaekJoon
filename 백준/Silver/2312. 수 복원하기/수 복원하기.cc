8ca049ef06d4e8d44955320a412713b2573cdf79
7aeHoon <108669890+JaeHoon97@users.noreply.github.com>
2025-02-09 03:29:21 +0900

    [Silver III] Title: 수 복원하기, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/2312. 수 복원하기/수 복원하기.cc
+++ 백준/Silver/2312. 수 복원하기/수 복원하기.cc
@@ -1,52 +1,61 @@
-#include <algorithm>
 #include <iostream>
-#include <map>
-#include <string>
 #include <vector>
-
+#include <map>
 using namespace std;
 
-int testCase;
-bool prime[100001];
+const int MAX_LIMIT = 100001;
+vector<bool> isPrime(MAX_LIMIT, true);
+vector<int> primes;
 
-void initPrime() {
-    fill(prime, prime + 100001, true);
-    prime[0] = prime[1] = false;
-
-    for (int i = 2; i < 100001; i++) {
-        if (!prime[i]) {
-            continue;
-        }
-        for (int j = i * 2; j < 100001; j += i) {
-            prime[j] = false;
+// 에라토스테네스의 체를 이용하여 소수를 구하고,
+// 소수 목록을 primes 벡터에 저장합니다.
+void initPrimes() {
+    isPrime[0] = isPrime[1] = false;
+    for (int i = 2; i < MAX_LIMIT; i++) {
+        if (isPrime[i]) {
+            primes.push_back(i);
+            // i의 제곱부터 시작하면 이미 작은 인수들에 의해 걸러진 상태입니다.
+            if ((long long)i * i < MAX_LIMIT) {
+                for (int j = i * i; j < MAX_LIMIT; j += i) {
+                    isPrime[j] = false;
+                }
+            }
         }
     }
 }
 
 int main() {
-    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    initPrime();
+    initPrimes();
 
+    int testCase;
     cin >> testCase;
-
-    for (int i = 0; i < testCase; i++) {
-        map<int, int> m;
-        int num, index = 2;
-
+    
+    while (testCase--) {
+        int num;
         cin >> num;
-
-        while (num != 1) {
-            if (prime[index] == true && num % index == 0) {
-                num /= index;
-                m[index]++;
-            } else {
-                index++;
+        map<int, int> factorCount;  // 소인수와 그 지수를 저장합니다.
+        int temp = num;
+        
+        // 미리 구한 소수 목록을 이용하여 소인수분해를 수행합니다.
+        for (int p : primes) {
+            if ((long long)p * p > temp) break;  // p가 temp의 제곱근보다 크면 더 이상 나눌 필요 없음
+            while (temp % p == 0) {
+                factorCount[p]++;
+                temp /= p;
             }
         }
-
-        for (pair<int, int> p : m) {
-            cout << p.first << ' ' << p.second << '\n';
+        
+        // 남은 temp가 1보다 크다면, 소수입니다.
+        if (temp > 1) {
+            factorCount[temp]++;
+        }
+        
+        // 소인수와 그 지수를 출력합니다.
+        for (const auto &entry : factorCount) {
+            cout << entry.first << " " << entry.second << "\n";
         }
     }
 
