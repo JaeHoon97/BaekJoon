9080bd7459e4a63cb72a0022fd41f8cd39647a40
7aeHoon <108669890+JaeHoon97@users.noreply.github.com>
2025-02-27 23:57:45 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.02 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 vector<int> solution(vector<string> wallpaper) {
-    vector<int> answer;
+    vector<int> answer = {100, 100, -1, -1};
     vector<int> xArr, yArr;
 
     int weight = wallpaper[0].size();  // 가로 크기
@@ -15,19 +15,13 @@ vector<int> solution(vector<string> wallpaper) {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < weight; j++) {
             if (wallpaper[i][j] == '#') {
-                xArr.push_back(j);
-                yArr.push_back(i);
+                answer[0] = min(i, answer[0]);      // y의 최소
+                answer[1] = min(j, answer[1]);      // x의 최소
+                answer[2] = max(i + 1, answer[2]);  // y의 최대
+                answer[3] = max(j + 1, answer[3]);  // x의 최대
             }
         }
     }
-
-    sort(xArr.begin(), xArr.end());
-    sort(yArr.begin(), yArr.end());
-
-    answer.push_back(yArr.front());
-    answer.push_back(xArr.front());
-    answer.push_back(yArr.back() + 1);
-    answer.push_back(xArr.back() + 1);
 
     return answer;
 }
